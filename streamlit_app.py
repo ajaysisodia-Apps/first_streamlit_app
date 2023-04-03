3de06fc7a6118bf1af7783543d8d82921a4d2bf5
ajaysisodia-Apps <129722824+ajaysisodia-Apps@users.noreply.github.com>
2023-04-03 02:46:48 -0400

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,10 @@ import streamlit
 streamlit.title('My Parent\'s New Healthy Diner')
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket smothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket smothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
 
 

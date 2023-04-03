15601ca3985611fb732a41985303e23d52050c7b
ajaysisodia-Apps <129722824+ajaysisodia-Apps@users.noreply.github.com>
2023-04-03 02:47:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit
 
 streamlit.title('My Parent\'s New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket smothie')
